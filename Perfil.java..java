cc1d34f19637f60e8ca15941c489eec08d340377
HenriqueLucas01 <lucasoliveira1327jj@gmail.com>
2025-02-24 21:24:36 -0300

    Add files via upload

---
+++ Perfil.java..java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nome: Lucas Henrique de Oliveira");
+        System.out.println("Idade: 19 Anos");
+        System.out.println("Matar porcos");
+    }
+}
